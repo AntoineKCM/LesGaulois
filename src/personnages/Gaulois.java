f0ab3d74242c7df1c05cfa099397e9ebd26d76e2
AntoineKCM <antoinedevaud2@gmail.com>
2026-01-08 08:45:06 +0100

    TP1 fin de l’exercice 1

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,14 @@
+package personnages;
+
+public class Gaulois {
+	private string nom ;
+	private int force ;
+	
+	public Gaulois(string nom,int force) {
+		this.nom = nom ;
+		this.force = force ;
+	}
+	public string getNom() {
+		return nom ; 
+	}
+}
